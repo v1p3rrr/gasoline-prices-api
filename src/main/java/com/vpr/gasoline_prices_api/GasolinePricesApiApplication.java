1de43cfdee198893cc77e138cb5968b69e3db5ac
v1p3rrr <cleverminer@gmail.com>
2023-04-29 19:21:39 +0300

    initial commit

---
+++ src/main/java/com/vpr/gasoline_prices_api/GasolinePricesApiApplication.java
@@ -0,0 +1,13 @@
+package com.vpr.gasoline_prices_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolinePricesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasolinePricesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vpr/gasoline_prices_api/GasolinePricesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpr.gasoline_prices_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasolinePricesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
